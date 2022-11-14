54bf7c269c3583d211aee6b9794df058449beac6
Brahim Berkasse <32434103+brkss@users.noreply.github.com>
2022-11-15 00:49:06 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by brkss on 15/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
