52ce3bcd11e48ced950a9229663c1d318c7a40f7
Brahim Berkasse <32434103+brkss@users.noreply.github.com>
2022-11-18 18:07:42 +0100

    🧹 set Landmark structure

---
+++ Landmarks/Landmarks/LandMark.swift
@@ -0,0 +1,36 @@
+//
+//  LandMark.swift
+//  Landmarks
+//
+//  Created by brkss on 18/11/2022.
+//
+
+import Foundation
+import SwiftUI
+import CodeLocation
+
+struct LandMark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)    
+    }
+    
+    private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude
+        )
+    }
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
