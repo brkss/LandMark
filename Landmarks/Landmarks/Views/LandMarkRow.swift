d8dfeafdbb5fbd325f041ee2b86459bc75be90bb
Brahim Berkasse <32434103+brkss@users.noreply.github.com>
2022-11-18 18:55:22 +0100

    🪛 add landmarkrow view

---
+++ Landmarks/Landmarks/Views/LandMarkRow.swift
@@ -0,0 +1,21 @@
+//
+//  LandMarkRow.swift
+//  Landmarks
+//
+//  Created by brkss on 18/11/2022.
+//
+
+import SwiftUI
+
+struct LandMarkRow: View {
+    var landmark : LandMark
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandMarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandMarkRow()
+    }
+}
